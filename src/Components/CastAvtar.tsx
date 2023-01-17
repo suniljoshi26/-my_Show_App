ef37809878aef0d0e7ff48b276ac8f81768c9a0a
Sunil Joshi <joshisunil2601@gmail.com>
2023-01-17 21:29:50 +0530

    casr

--- src/Components/CastAvtar.tsx
+++ src/Components/CastAvtar.tsx
@@ -14,7 +14,7 @@ const CastAvtar: FC<CastAvtarProps> = ({ cast, className }) => {
   return (
     <div
       className={
-        "md:w-52 w-32 bg-white border border-gray-500 rounded-md space-x-1 z-50  p-1" +
+        "md:w-52 w-32 bg-white border border-gray-700 rounded-md space-x-1 z-50  p-1" +
         className
       }
     >
@@ -22,7 +22,7 @@ const CastAvtar: FC<CastAvtarProps> = ({ cast, className }) => {
         return (
           <div
             key={p.id}
-            className="flex items-center justify-between md:px-4 md:py-1 border-b-2 border-gray-500 "
+            className="flex items-center justify-between md:px-4 md:py-1 border-b-2 border-black "
           >
             <Avatar
               alt="Remy Sharp"
